80ede712eda5c0a7767017cb6d8aaa1de6cc52d4
Kamran <kmrn.babirov@gmail.com>
2024-11-18 00:19:06 +0100

    New

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-let menuIcon = document.querySelector("menuIcon");
-let navbar =document.querySelector(".navbar");
+const menuIcon = document.querySelector("#menu-icon");
+const navbar = document.querySelector(".navbar");
 
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle("bx-x");
+menuIcon.addEventListener("click", () => {
     navbar.classList.toggle("active");
-}
+});
